5af9947817d4fb0efbeafadc2d79e06c940c86ee
JosePu1201 <joseluis-pusam@cunoc.edu.gt>
2024-03-14 02:03:36 -0600

    incio de fron y moldes para hacer cambios futuros

---
+++ View/footer.php
@@ -0,0 +1,37 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="https://www.facebook.com/niceaccesoriosdetalles" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="https://maps.app.goo.gl/yADTyWR9L5hxbXQV8" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">¿Por qué elegirnos?</h4>
+               <p>
+    Nos enorgullece ofrecerte la mejor selección de accesorios para que te sientas bellamente única. Nos destacamos por:
+    <span style="display: block;">- Variedad Exclusiva.</span>
+    <span style="display: block;">- Precios Competitivos.</span>
+    <span style="display: block;">- Calidad Inigualable.</span>
+    Descubre la diferencia en NICE ACCESORIOS: ¡tu destino para realzar tu belleza con estilo y asequibilidad!
+</p>
+
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Quetzaltenango</p>
+                <p style="color: #FFF">21 Av 6-79 zona 3</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  +502 – 4848 0146 / +502 – 4743 4079</p></br>
+                E-mail: <a href="eusanavi20@hotmail.com" target="_blank" style="color: #5bc0de"> &nbsp; eusanavi20@hotmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">NICE ACCESORIOS &copy; 2023</h5>
+</footer>
